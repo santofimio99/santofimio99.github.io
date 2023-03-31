d6c9a3dd6e3ab3f48bbee31df1106891f200b647
santofimio99 <129488254+santofimio99@users.noreply.github.com>
2023-03-31 02:00:12 -0500

    creando mi primer sitio web

---
+++ enviar.php
@@ -0,0 +1,12 @@
+<?php
+  $fecha = $_POST['fecha'];
+  $hora = $_POST['hora'];
+  $cantidad = $_POST['cantidad'];
+
+  // Aquí puedes hacer lo que quieras con la información del formulario,
+  // como almacenarla en una base de datos o enviarla por correo electrónico.
+
+  echo "Fecha: $fecha<br>";
+  echo "Hora: $hora<br>";
+  echo "Cantidad de personas: $cantidad<br>";
+?>
